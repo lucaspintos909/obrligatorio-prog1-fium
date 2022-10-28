dcb8d4b81f116f224eca887268488a57c6fd8ac1
lucaspintos909 <lucaspintos909@gmail.com>
2022-10-28 19:40:29 -0300

    Se agrega clase "Equipo"

---
+++ equipo.py
@@ -0,0 +1,18 @@
+
+class Equipo:
+    def __init__(self, nombre: str, categorias: list, desarrolladores: list):
+        self._nombre = nombre
+        self._categorias = categorias
+        self._desarrolladores = desarrolladores
+    
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @property
+    def categorias(self):
+        return self._categorias
+
+    @property
+    def desarrolladores(self):
+        return self._desarrolladores
