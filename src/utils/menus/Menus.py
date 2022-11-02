3e137981cb15e16a4d086095acf66fe57f562faa
lucaspintos909 <lucaspintos909@gmail.com>
2022-11-02 18:13:40 -0300

    Modulo para los menús

---
+++ src/utils/menus/Menus.py
@@ -0,0 +1,11 @@
+menu_principal = """
+------------------------------
+| Menú principal         - x |
+------------------------------
+|  1. Alta de desarrollador  |
+|  2. Alta de videojuego     |
+|  3. Simular competencia    |
+|  4. Realizar consultas     |
+|  5. Salir                  |
+------------------------------
+"""
