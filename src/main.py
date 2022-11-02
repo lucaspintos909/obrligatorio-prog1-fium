a9c9a25807bc6fdd0c69511456850ee29d4993f3
lucaspintos909 <lucaspintos909@gmail.com>
2022-11-02 17:57:46 -0300

    Poniendo fino el menú

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
-opciones_menu="""
-------------------
-| Menú Principal |
-------------------
-Seleccione alguna opción del manú:
-    1. Alta de desarrollador
-    2. Alta de videojuego
-    3. Simular competencia
-    4. Realizar consultas
-    5. Finalizar programa
+import utils.MenuUtils as MenuUtils
 
+opciones_menu = """
+------------------------------
+| Menú principal         - x |
+------------------------------
+|  1. Alta de desarrollador  |
+|  2. Alta de videojuego     |
+|  3. Simular competencia    |
+|  4. Realizar consultas     |
+|  5. Salir                  |
+------------------------------
 """
 def menu():
-    option = None
-    while option != "5":
+    opcion = None
+    while opcion != "5":
         print(opciones_menu)
-        option = input("Opción: ")
-        match(option):
+        opcion = input("Opción: ")
+        MenuUtils.limpiar_consola()
+        match opcion:
             case "1":
                 print("Alta de desarrollador")
             case "2":
@@ -25,8 +27,12 @@ def menu():
             case "4":
                 print("Realizar consultas")
             case "5":
+                input("Presione la tecla enter para salir...")
                 print("Chau!")
-            case other:
+                exit()
+            case cualquier_otro:
                 print("Opción inválida, intente nuevamente.")
 
+
+MenuUtils.limpiar_consola()
 menu()
