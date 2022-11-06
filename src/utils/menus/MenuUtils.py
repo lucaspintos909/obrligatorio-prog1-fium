72a1985cd94ba04ee0f68cee7733bb13cef40fb5
lucaspintos909 <lucaspintos909@gmail.com>
2022-11-06 19:02:03 -0300

    Mostrando mejor el error al usuario

--- src/utils/menus/MenuUtils.py
+++ src/utils/menus/MenuUtils.py
@@ -7,3 +7,17 @@ def limpiar_consola():
     # Para mac y linux
     else:
         system('clear')
+
+def pedir_dato(setter, string_input: str):
+    dato_ok = False
+    while not dato_ok:
+        dato_ingresado = input(f"|  {string_input}")
+        try:
+            setter(dato_ingresado)
+            dato_ok = True
+            
+        except Exception as error:
+            print(f"|  ***** ")
+            print(f"|  ERROR: {error}, intente nuevamente.")
+            print(f"|  ***** ")
+            print(f"|  ")
