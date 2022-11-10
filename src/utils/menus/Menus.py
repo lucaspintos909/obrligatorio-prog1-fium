e7634f8eeb22be9aa5c0c35c99f377f45369f50f
lucaspintos909 <lucaspintos909@gmail.com>
2022-11-10 13:36:37 -0300

    Menu de alta de videojuego

--- src/utils/menus/Menus.py
+++ src/utils/menus/Menus.py
@@ -1,17 +1,23 @@
 menu_principal = """
-------------------------------
-| Menú principal         - x |
-------------------------------
-|  1. Alta de desarrollador  |
-|  2. Alta de videojuego     |
-|  3. Simular competencia    |
-|  4. Realizar consultas     |
-|  5. Salir                  |
-------------------------------
+---------------------------------------
+| Menú principal                  - x |
+---------------------------------------
+|  1. Alta de desarrollador           |
+|  2. Alta de videojuego              |
+|  3. Simular competencia             |
+|  4. Realizar consultas              |
+|  5. Salir                           |
+---------------------------------------
 """
 
 menu_alta_desarrollador = """
 ---------------------------------------
 | > Alta de desarrollador         - x |
+---------------------------------------
+"""
+
+menu_alta_videojuego = """
+---------------------------------------
+| > Alta de videojuego            - x |
 ---------------------------------------
 """
