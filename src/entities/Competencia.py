12fc876121daede6ef468c3e59f6b828ed5999f2
lucaspintos909 <lucaspintos909@gmail.com>
2022-11-11 11:59:11 -0300

    Desarrolladores y videojuegos en competencia

--- src/entities/Competencia.py
+++ src/entities/Competencia.py
@@ -1,8 +1,22 @@
 
 class Competencia:
 
-    def __init__(self, categoria: str) -> None:
+    def __init__(self) -> None:
         self._videojuegos: list = []
-        self._categoria: str = categoria
-        self._podio: list = []
-        
+        self._desarrolladores: list = []
+
+    @property
+    def videojuegos(self):
+        return self._videojuegos
+    
+    @videojuegos.setter
+    def videojuegos(self, videojuegos):
+        self._videojuegos = videojuegos
+    
+    @property
+    def desarrolladores(self):
+        return self._desarrolladores
+    
+    @desarrolladores.setter
+    def desarrolladores(self, desarrolladores):
+        self._desarrolladores = desarrolladores
