2f7a5935a21f0f4d9718b570c95242a7a0b61b00
lucaspintos909 <lucaspintos909@gmail.com>
2022-11-11 11:00:56 -0300

    Renombrando propiedad videojuegos

--- src/entities/Competencia.py
+++ src/entities/Competencia.py
@@ -2,7 +2,7 @@
 class Competencia:
 
     def __init__(self, categoria: str) -> None:
-        self._juegos: list = []
+        self._videojuegos: list = []
         self._categoria: str = categoria
         self._podio: list = []
         
