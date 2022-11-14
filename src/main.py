07a32a48fe29e690aa9096ac22ae2c8919eceed6
lucaspintos909 <lucaspintos909@gmail.com>
2022-11-14 01:00:50 -0300

    Funcionalidad para realizar consultas termianda

--- src/main.py
+++ src/main.py
@@ -26,6 +26,7 @@ def menu():
                 competencia.desarrolladores += [desarrollador]
 
                 guardar_desarrolladores(competencia.desarrolladores)
+
             case "2":
                 videojuego = Videojuego()
                 videojuego.menu_de_alta(competencia.desarrolladores)
@@ -36,30 +37,57 @@ def menu():
 
             case "4":
                 devs = competencia.desarrolladores.copy()
-                """ def por_exp(dev):
-                    return dev.experiencia
-                devs.sort(key=por_exp, reverse=True)
-                dict_perro= {}
-                for index, dev  in enumerate(devs[0:10]):
-                    dict_perro[index + 1] = f"{dev.ci} - {dev.nombre} - Experiencia: {dev.experiencia}" """
-
-                def verificar_por_rol(dev):
-                    return dev.rol == "Programador"
-
-                devs = list(filter(verificar_por_rol, devs))
-
+                """ 10 mejores desarrolladores registrados """
                 def por_exp(dev):
                     return dev.experiencia
 
-                devs.sort(key=por_exp, reverse=True)
+                devs.sort(key=lambda dev: dev.experiencia, reverse=True)
+                top_10_devs_por_exp = {}
+                for index, dev in enumerate(devs[0:10]):
+                    top_10_devs_por_exp[index +
+                                        1] = f"{dev.ci} - {dev.nombre} - Experiencia: {dev.experiencia}"
 
-                dict_gato = {}
+                """  5 mejores desarrolladores con el rol de Programador """
+                programadores = competencia.desarrolladores.copy()
 
-                for index, dev in enumerate(devs[0:5]):
-                    dict_gato[index +
-                              1] = f"{dev.ci} - {dev.nombre} - Experiencia: {dev.experiencia}"
+                # Obtengo todos los devs con rol "Programador"
+                programadores = list(filter(lambda dev: dev.rol == "Programador", devs))
 
-                print(dict_gato)
+                # Ordeno la lista de desarrolladores por su experiencia
+                programadores.sort(key=lambda dev: dev.experiencia, reverse=True)
+
+                top_5_programadores = {}
+                
+                for index, dev in enumerate(programadores[0:5]):
+                    top_5_programadores[index +
+                                        1] = f"{dev.ci} - {dev.nombre} - Experiencia: {dev.experiencia}"
+
+                """ 7 desarrolladores registrados con edad más avanzada """
+                devs_con_mas_edad = competencia.desarrolladores.copy()
+                # Ordeno la lista de desarrolladores por su edad
+                devs_con_mas_edad.sort(key=lambda dev: dev.edad, reverse=True)
+                top_7_con_mas_edad = {}
+                
+                for index, dev in enumerate(devs_con_mas_edad[0:15]):
+                    top_7_con_mas_edad[index +
+                                        1] = f"{dev.ci} - {dev.nombre} - Edad: {dev.edad}"
+
+                """ videojuego con más desarrolladores que provienen de Uruguay """
+                videojuegos = competencia.videojuegos.copy()
+                
+                videojuegos_cant_uruguayos = []
+                
+                for juego in videojuegos:
+                    
+                    devs_uruguayos = filter(lambda dev: dev.pais_origen == "Uruguay", juego.desarrolladores)
+                    cant_uruguayos = len(list(devs_uruguayos))
+                    info_juego = {
+                        "juego": juego,
+                        "cant_uruguayos": cant_uruguayos
+                    }
+                    videojuegos_cant_uruguayos.append(info_juego)
+                videojuegos_cant_uruguayos.sort(key=lambda juego: juego["cant_uruguayos"], reverse=True)
+
             case "5":
                 raise KeyboardInterrupt()
             case cualquier_otro:
